4c2673377229b200a64ccb151835109b60148a74
TJ Schuler <tschuler@paloaltonetworks.com>
2020-04-11 10:59:52 -0600

    Fixing liniting errors

--- deploy_panorama/deploy_panorama.py
+++ deploy_panorama/deploy_panorama.py
@@ -1,13 +1,13 @@
 import sys
-from datetime import datetime
+# from datetime import datetime
 import time
 import os
 import socket
 import requests
 import json
 from pathlib import Path
-from requests.exceptions import ConnectionError
-from requests import get
+# from requests.exceptions import ConnectionError
+# from requests import get
 from docker import DockerClient
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -17,6 +17,7 @@ from cryptography.hazmat.backends import default_backend as crypto_default_backe
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 # Function to convert seconds to a Hours: Minutes: Seconds display
 def convert(seconds):
     min, sec = divmod(seconds, 60)
@@ -24,20 +25,24 @@ def convert(seconds):
     return '%d:%02d:%02d' % (hour, min, sec)
 
 
-# Pull in the AWS Provider variables. These are set in the Skillet Environment and are hidden variables so the user doesn't need to adjust them everytime.
+# Pull in the AWS Provider variables. These are set in the Skillet Environment and are hidden variables so the
+# user doesn't need to adjust them everytime.
 variables = dict(AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID'),
                  AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY'), TF_IN_AUTOMATION='True')
 variables.update(TF_VAR_deployment_name=os.environ.get('DEPLOYMENT_NAME'), TF_VAR_vpc_cidr_block=os.environ.get(
-                'vpc_cidr_block'), TF_VAR_enable_ha=os.environ.get('enable_ha'), TF_VAR_aws_region=os.environ.get('AWS_REGION'))
-# A variable the defines if we are creating or destroying the environment via terraform. Set in the dropdown on Panhandler.
+                'vpc_cidr_block'), TF_VAR_enable_ha=os.environ.get('enable_ha'),
+                TF_VAR_aws_region=os.environ.get('AWS_REGION'))
+# A variable the defines if we are creating or destroying the environment via terraform. Set in the dropdown
+# on Panhandler.
 tfcommand = (os.environ.get('Init'))
 
 # Define the working directory for the container as the terraform directory and not the directory of the skillet.
 path = Path(os.getcwd())
 wdir = str(path.parents[0])+'/terraform/aws/panorama/'
 
-# If the variable is defined for the script to automatically determine the public IP, then capture the public IP and add it to the Terraform variables.
-# If it isn't then add the IP address block the user defined and add it to the Terraform variables.
+# If the variable is defined for the script to automatically determine the public IP, then capture the public IP
+# and add it to the Terraform variables. If it isn't then add the IP address block the user defined and add it
+# to the Terraform variables.
 if (os.environ.get('specify_network')) == 'auto':
     # Using verify=false in case the container is behind a firewall doing decryption.
     ip = requests.get('https://api.ipify.org', verify=False).text+'/32'
@@ -45,14 +50,15 @@ if (os.environ.get('specify_network')) == 'auto':
 else:
     variables.update(TF_VAR_onprem_IPaddress=(os.environ.get('onprem_cidr_block')))
 
-# The script uses a terraform docker container to run the terraform plan. The script uses the docker host that panhandler is running on to run the new 
-# conatiner. /var/lib/docker.sock must be mounted on panhandler
+# The script uses a terraform docker container to run the terraform plan. The script uses the docker host that
+# panhandler is running on to run the new conatiner. /var/lib/docker.sock must be mounted on panhandler
 client = DockerClient()
 
 # If the variable is set to apply then create the environment and check for Panorama availabliity
 if tfcommand == 'apply':
-    # Generate a new RSA keypair to use to SSH to the VM. If you are using your own automation outside of Panhandler then you should use your own keys.
-    if os.path.exists(wdir+'id_rsa') != True:
+    # Generate a new RSA keypair to use to SSH to the VM. If you are using your own automation outside of
+    # Panhandler then you should use your own keys.
+    if os.path.exists(wdir+'id_rsa') is not True:
         print('Generating Crypto Key')
         key = rsa.generate_private_key(
             backend=crypto_default_backend(),
@@ -71,32 +77,36 @@ if tfcommand == 'apply':
             pubfile.write(public_key)
         # Add the public key to the variables sent to Terraform so it can create the AWS key pair.
         variables.update(TF_VAR_ra_key=public_key)
-    # If the keys already exist don't recreate them or else you might not be able to access a resource you previously created but havent set the password on.
+    # If the keys already exist don't recreate them or else you might not be able to access a resource you
+    # previously created but havent set the password on.
     else:
         print('Crypto Key exists already, skipping....')
         public_key = open(wdir+'pub', 'r')
         # Add the public key to the variables sent to Terraform so it can create the AWS key pair.
         variables.update(TF_VAR_ra_key=public_key.read())
 
-    # Init terraform with the modules and providers. The continer will have the some volumes as Panhandler. This allows it to access
-    # the files Panhandler downloaded from the GIT repo. 
+    # Init terraform with the modules and providers. The continer will have the some volumes as Panhandler.
+    # This allows it to access the files Panhandler downloaded from the GIT repo.
     container = client.containers.run('hashicorp/terraform:light', 'init -no-color -input=false', auto_remove=True,
-                                      volumes_from=socket.gethostname(), working_dir=wdir, environment=variables, detach=True)
-    # Monitor the log so that the user can see the console output during the run versus waiting until it is complete. The container stops and
-    # is removed once the run is complete and this loop will exit at that time.
+                                      volumes_from=socket.gethostname(), working_dir=wdir,
+                                      environment=variables, detach=True)
+    # Monitor the log so that the user can see the console output during the run versus waiting until it is complete.
+    # The container stops and is removed once the run is complete and this loop will exit at that time.
     for line in container.logs(stream=True):
         print(line.decode('utf-8').strip())
     # Run terraform apply
     container = client.containers.run('hashicorp/terraform:light', 'apply -auto-approve -no-color -input=false',
-                                      auto_remove=True, volumes_from=socket.gethostname(), working_dir=wdir, environment=variables, detach=True)
-    # Monitor the log so that the user can see the console output during the run versus waiting until it is complete. The container stops and
-    # is removed once the run is complete and this loop will exit at that time.
+                                      auto_remove=True, volumes_from=socket.gethostname(), working_dir=wdir,
+                                      environment=variables, detach=True)
+    # Monitor the log so that the user can see the console output during the run versus waiting until it is complete.
+    #  The container stops and is removed once the run is complete and this loop will exit at that time.
     for line in container.logs(stream=True):
         print(line.decode('utf-8').strip())
 
     # Capture the IP addresses of Panorama using Terraform output
     eip = json.loads(client.containers.run('hashicorp/terraform:light', 'output -json -no-color', auto_remove=True,
-                                           volumes_from=socket.gethostname(), working_dir=wdir, environment=variables).decode('utf-8'))
+                                           volumes_from=socket.gethostname(), working_dir=wdir,
+                                           environment=variables).decode('utf-8'))
     panorama_ip = (eip['primary_eip']['value'])
 
     # Inform the user of Panorama's external IP address
@@ -108,7 +118,8 @@ if tfcommand == 'apply':
         secondary_ip = (eip['secondary_eip']['value'])
         print('The Secondary Panorama IP address is '+secondary_ip)
 
-    # Panorama is deployed but it isn't ready to be configured until it is fully booted. Check for that state by trying to reach the web page.
+    # Panorama is deployed but it isn't ready to be configured until it is fully booted. Check for that state by trying
+    # to reach the web page.
     print('')
     print('Checking if Panorama is fully booted. This can take 30 minutes or more...')
 
@@ -118,12 +129,12 @@ if tfcommand == 'apply':
         try:
             request = requests.get(
                 'https://'+panorama_ip, verify=False, timeout=5)
-        except requests.ConnectionError as e:
+        except requests.ConnectionError:
             print('Panorama is still booting.... ['+convert(temptime)+'s elapsed]')
             time.sleep(5)
             temptime = temptime+10
             continue
-        except requests.Timeout as e:
+        except requests.Timeout:
             print('Timeout Error')
             time.sleep(5)
             temptime = temptime+10
@@ -141,12 +152,12 @@ if tfcommand == 'apply':
             try:
                 request = requests.get(
                     'https://'+secondary_ip, verify=False, timeout=5)
-            except requests.ConnectionError as e:
+            except requests.ConnectionError:
                 print('The Secondary Panorama is still booting.... ['+convert(temptime)+'s elapsed]')
                 time.sleep(5)
                 temptime = temptime+10
                 continue
-            except requests.Timeout as e:
+            except requests.Timeout:
                 print('Timeout Error')
                 time.sleep(5)
                 temptime = temptime+10
@@ -162,9 +173,10 @@ if tfcommand == 'apply':
 # If the variable is destroy, then destroy the environment and remove the SSH keys.
 elif tfcommand == 'destroy':
     container = client.containers.run('hashicorp/terraform:light', 'destroy -auto-approve -no-color -input=false',
-                                      auto_remove=True, volumes_from=socket.gethostname(), working_dir=wdir, environment=variables, detach=True)
-    # Monitor the log so that the user can see the console output during the run versus waiting until it is complete. The container stops and 
-    # is removed once the run is complete and this loop will exit at that time.
+                                      auto_remove=True, volumes_from=socket.gethostname(), working_dir=wdir,
+                                      environment=variables, detach=True)
+    # Monitor the log so that the user can see the console output during the run versus waiting until it is complete.
+    # The container stops and is removed once the run is complete and this loop will exit at that time.
     for line in container.logs(stream=True):
         print(line.decode('utf-8').strip())
     # Remove the SSH keys we used to provision Panorama from the container.
@@ -172,7 +184,7 @@ elif tfcommand == 'destroy':
     try:
         os.remove(wdir+'pub')
         os.remove(wdir+'id_rsa')
-    except:
+    except Exception:
         print('  There where no keys to remove')
 
 sys.exit(0)
